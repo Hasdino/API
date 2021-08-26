705031a59a551d948e6242f1b0cb88a9abc1713f
Henrique Afonso <henrique8afonso@gmail.com>
2021-08-26 23:25:00 +0100

    daskjnd

--- main.py
+++ main.py
@@ -1,64 +1,46 @@
-from flask import Flask
+from flask import Flask,redner_template,url_for,flash,redirect
 from flask_sqlalchemy import SQLAlchemy
 import hiddenthings
-import pymysql
-from flask import Flask, render_template, flash, request, redirect, url_for
-from forms import LoginForm
-from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
 
 
 
-app=Flask(__name__)
+app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{}:{}@{}/{}".format(hiddenthings.dbuser,hiddenthings.dbpass,hiddenthings.dbhost,hiddenthings.dbname)
 db=SQLAlchemy(app)
 
 
-#our model
-class UserInfo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(100), unique = True)
-    password = db.Column(db.String(100))
- 
- 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
- 
- 
- 
- 
- 
- 
- 
-#creating our routes
-@app.route('/')
+
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(80), unique=True,nullable=True)
+    email = db.Column(db.String(120), unique=True)
+    image_file=db.Column(db.String(20), nullable=True, default="default.jpg")
+    password= db.Column(db.String(60),nullable=False)
+
+
+    def __repr__(self):
+        return f"User('{self.name}', 'self{self.email}' , '{self.image_file}')"
+
+
+
+
+
+@app.route("/")
 def index():
+    return "<h1 style='color : red'>hello flask</h1>"
+
+
+
+@app.get("/drinks")
+def get_drinks():
+    return "yo"
  
-    return render_template('index.html')
- 
- 
- 
-#login route
-@app.route('/login' , methods = ['GET', 'POST'])
-def Login():
-    form = LoginForm()
- 
-    if form.validate_on_submit():
-        if request.form['username'] != 'codeloop' or request.form['password'] != '12345':
-            flash("Invalid Credentials, Please Try Again")
- 
- 
-        else:
-            return redirect(url_for('index'))
- 
- 
- 
-    return render_template('login.html', form = form)
- 
- 
- 
- 
- 
-#run flask app
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
